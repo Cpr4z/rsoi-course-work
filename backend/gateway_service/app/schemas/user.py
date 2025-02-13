0b52e46662029c27c7dbc1bf7681ac2d6a4bf033
cpr4z <matvey.agarkov02@gmail.com>
2025-02-13 22:23:54 +0300

    add lab3

---
+++ backend/gateway_service/app/schemas/user.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel
+from schemas.bonus import PrivilegeShortInfo
+from schemas.ticket import TicketResponse
+
+class UserInfoResponse(BaseModel):
+    tickets: list[TicketResponse]
+    privilege: PrivilegeShortInfo | dict[None, None]
