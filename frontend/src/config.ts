4591c944e1057f24db1f9398cc13206e06a672ec
cpr4z <matvey.agarkov02@gmail.com>
2025-04-23 00:10:19 +0300

    fix build

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,27 +1,27 @@
 export const config = {
     server: {
         auth: {
-            host: "51.250.36.177",
+            host: "158.160.176.74",
             port: 8888,
         },
         statistics: {
-            host: "51.250.36.177",
+            host: "158.160.176.74",
             port: 8090,
         },
         gateway: {
-            host: "51.250.36.177",
+            host: "158.160.176.74",
             port: 8080,
         },
         ticket: {
-            host: "51.250.36.177",
+            host: "158.160.176.74",
             port: 8070,
         },
         flight: {
-            host: "51.250.36.177",
+            host: "158.160.176.74",
             port: 8060,
         },
         bonus: {
-            host: "51.250.36.177",
+            host: "158.160.176.74",
             port: 8050,
         },
     },
